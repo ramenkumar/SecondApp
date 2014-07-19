a09c8f00c1a4268de9422d9612369cd3bc4b9308
ramenbhar <ramen.bhar@co-operative.coop>
2014-07-20 00:44:34 +0530

    test

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok("Hi This is your second application");
+        return ok("Hi This is your second application ");
     }
 
 }
